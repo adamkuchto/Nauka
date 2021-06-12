816078c64ff1032df1b0611170eb7b01e8137254
Adam Kuchto <adamkuchto@gmail.com>
2021-06-12 19:57:50 +0200

    zadania na spoj

---
+++ spoj.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+string = "pn" , "wt","sr", "cw","pt","so","nd"};
+int test;
+
+int main()
+{
+    if(test>0)
+    {
+
+      test--;  
+    }
+    cout << tab[1] << endl;
+    
+
+    return 0;
+}
