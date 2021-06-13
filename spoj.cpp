5241ed438ba1382fcadcb40c7ab850f025df248e
Adam Kuchto <adamkuchto@gmail.com>
2021-06-13 08:34:52 +0200

    zadanie z ksiazki

--- spoj.cpp
+++ spoj.cpp
@@ -1,19 +1,84 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-string = "pn" , "wt","sr", "cw","pt","so","nd"};
-int test;
+double input;
 
 int main()
 {
-    if(test>0)
+  /*
+  cin >> input;           logarytm naturalny
+  cout << log(input);
+  */
+  /*
+  cin >> input;
+  int p = M_PI * pow(input,2);
+  cout << p << endl;
+*/
+  /*
+  cin >> input;
+  int natural,
+      dziesietny;
+
+  natural = log(input); // 4 = 1.39
+  dziesietny = log10(input); // 4 = 0.60
+  if (natural > dziesietny)
+  {
+    cout << "naturalny jest wiekszy" << endl;
+  }
+  else
+  {
+    cout << "dziesietny jest wiekszy" << endl;
+  }
+  */
+/*
+  int podstawa, wykladnik;
+  cin >> podstawa;
+  cin >> wykladnik;
+
+  cout << pow(podstawa, 1.0 / wykladnik) << endl;
+*/
+  
+/*
+  for (int i = 0; i < 5; i++)
+  {
+
+    if (i == 0)
     {
-
-      test--;  
+      cout << sin(0*M_PI/180) << endl; //0
     }
-    cout << tab[1] << endl;
-    
+    else if (i == 1)
+    {
+      cout << sin(30*M_PI/180) << endl; //0.5
+    }
+    else if (i == 2)
+    {
+      cout << sin(45*M_PI/180) << endl; // 0.7
+    }
+    else if (i == 3)
+    {
+      cout << sin(60*M_PI/180) << endl; // 0.86
+    }
+    else if (i == 4)
+    {
+      cout << sin(90*M_PI/180) << endl; // 1
+    }
+  }
+ */
+ /* 
+ cin >> input;
+ cout << round(input) << endl;  // 5 
+ cout << ceil(input) << endl;   // 6
+ cout << floor(input) << endl;  // 5
+*/
+  
+  double x, y;
+  cin >> x >> y;
 
-    return 0;
+  cout << x + y << endl; // 4
+  cout << x - y << endl; // 0
+  cout << x * y << endl; // 4
+  cout << x / y << endl; // 1
+
+  return 0;
 }
