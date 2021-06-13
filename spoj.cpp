bf67fdc2095c30831bfc5f2deed8cc4c6507e086
Adam Kuchto <adamkuchto@gmail.com>
2021-06-13 10:45:41 +0200

    stringi i tablice

--- spoj.cpp
+++ spoj.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 double input;
@@ -31,15 +32,15 @@ int main()
     cout << "dziesietny jest wiekszy" << endl;
   }
   */
-/*
+  /*
   int podstawa, wykladnik;
   cin >> podstawa;
   cin >> wykladnik;
 
   cout << pow(podstawa, 1.0 / wykladnik) << endl;
 */
-  
-/*
+
+  /*
   for (int i = 0; i < 5; i++)
   {
 
@@ -65,13 +66,13 @@ int main()
     }
   }
  */
- /* 
+  /* 
  cin >> input;
  cout << round(input) << endl;  // 5 
  cout << ceil(input) << endl;   // 6
  cout << floor(input) << endl;  // 5
 */
-  
+  /*
   double x, y;
   cin >> x >> y;
 
@@ -79,6 +80,53 @@ int main()
   cout << x - y << endl; // 0
   cout << x * y << endl; // 4
   cout << x / y << endl; // 1
+*/
+  /*
+  string tekst;
+  cin >> tekst;
+  int ile = tekst.length();
+
+  for (int i = 1; i <= ile; i++)
+  {
+    cout << tekst[ile - i] << endl;
+  }
+*/
+  /*
+  string tekst;
+  cin >> tekst;
+  int ile = tekst.length();
+
+  for (int i = 0; i <= ile; i++)
+  {
+    if (i % 2 == 0)
+    {
+      cout << tekst[i] << endl;
+    }
+  }
+  */
+  /*
+  const int CAPACITY = 20;
+  int tab[CAPACITY] = {5, 3, 7, 8, 98, 3, 23, 56, 23, 6, 5, 43, 6, 3, 4, 7}; // mozna zastosowac srand i szukac pseudolosowych lotto
+  int input;
+  string find = "nie";
+  cin >> input;
+
+  for (int i = 0; i < CAPACITY; i++)
+  {
+    if (tab[i] == input)
+    {
+      find = "tak";
+    }
+  }
+  if (find == "tak")
+  {
+    cout << "liczba znajduje sie w zbiorze " << endl;
+  }
+  else
+  {
+    cout << "brak liczby w zbiorze " << endl;
+  }
+  */
 
   return 0;
 }
